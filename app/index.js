9c53ca87acc740fa70d0ac4753f33cf010da5627
Ben Bowes <bb@benbowes.com>
2016-08-14 10:29:01 +1000

    Update demo to mimic readme of get-or-else

--- app/index.js
+++ app/index.js
@@ -2,22 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getOrElse from 'get-or-else';
 
-const data = {
+const nameObj = {
   salutation: 'Mr',
   name: {
     first: 'James'
   }
 };
 
-const salutation = getOrElse({ get: [data,'salutation'], else: false });
+const salutation = getOrElse({  get: [nameObj,'salutation'], else: false });
 
 const NameComponent = () => {
   return (
     <h1>
-      We've been expecting you
+      We have been expecting you
+
       {salutation && <span> {salutation}</span>}
-      <span> {getOrElse({ get:[data,'name.first'], else:''})}</span>
-      <span> {getOrElse({ get:[data,'name.last'], else:''})}</span>
+
+      <span> {getOrElse({ get: [nameObj,'name.first'], else: '' })}</span>
+
+      <span> {getOrElse({ get: [nameObj,'name.last'], else: '' })}</span>
     </h1>
   );
 };
